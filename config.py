316864291e8579dd0484cb49bd37296e8b6a0a99
Yanohi7 <viktor.khudaska.22@pnu.edu.ua>
2025-02-08 18:49:56 +0200

    config.py changed for safety

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")  # ключ з .flaskenv або резервне значення
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback_secret")
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OUATH2_CLIENT_ID = os.getenv("OUATH2_CLIENT_ID")
+    OUATH2_CLIENT_SECRET = os.getenv("OUATH2_CLIENT_SECRET")
+    OUATH2_META_URL = os.getenv("OUATH2_META_URL")
